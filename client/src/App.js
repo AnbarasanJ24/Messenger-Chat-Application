07a304a07929787b8c0a718705ce88596747b485
Anbarasan <anbarasanj24@gmail.com>
2022-08-07 08:39:11 +0530

    Completed Initial setup for Login

--- client/src/App.js
+++ client/src/App.js
@@ -1,8 +1,14 @@
+import Dashboard from "./components/dashboard/Dashboard";
+import Login from "./components/login/Login";
+import useLocalStorage from './hooks/useLocalStorage';
 
 function App() {
+
+  const [id,setId] = useLocalStorage('id');
+
   return (
     <div className="App">
-      Hello
+      {id ? <Dashboard /> : <Login setId={setId} />}
     </div>
   );
 }
